3750892e7eea42c394ca91509cc5d715be83f067
SanjatulHasanSiam <sanjatulhasansiam.iit@gmail.com>
2023-08-26 12:27:17 +0600

    Shipper added

---
+++ NorthWind/Controllers/ShippersController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NorthWind.Models;
+
+namespace NorthWind.Controllers
+{
+    public class ShippersController : Controller
+    {
+        private readonly NorthwindContext _context;
+        public ShippersController(NorthwindContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var spippers = await _context.Shippers.ToListAsync();
+            return View(spippers);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Shipper model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = await _context.Shippers.AddAsync(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = await _context.Shippers.FindAsync(id);
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Shipper model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            _context.Shippers.Update(model);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var model = await _context.Shippers.FindAsync(id);
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(Shipper model)
+        {
+
+            _context.Shippers.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
