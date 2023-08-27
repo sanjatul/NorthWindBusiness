4f7f4cf3927c236b837f9d834e822ff34f7b9206
SanjatulHasanSiam <sanjatulhasansiam.iit@gmail.com>
2023-08-27 16:05:12 +0600

    Comments removed

--- NorthWind/wwwroot/js/topbuyers.js
+++ NorthWind/wwwroot/js/topbuyers.js
@@ -1,26 +1,4 @@
-﻿//For Buyers table
-//var dataTable;
-
-//$(document).ready(function () {
-//    loadDataTable();
-//});
-
-//function loadDataTable() {
-//    dataTable = $('#tblData').DataTable({
-//        "ajax": { url: '/home/getall' },
-//        "columns": [
-//            { data: 'orderId' },
-//            { data: 'customerName' },
-//            { data: 'productName' },
-//            { data: 'orderDate' },
-//            { data: 'quantity' },
-//            { data: 'price' },
-//            { data: 'amount' }
-//        ]
-//    });
-//}
-
-
+﻿
 
 $(document).ready(function () {
     $('#tblData').DataTable({
